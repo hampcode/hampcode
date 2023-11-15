af0771eb945e8433be1043ec509f26ed43e61b4f
Henry Antonio Mendoza Puerta <devacademyweb@gmail.com>
2023-11-14 22:26:33 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,83 @@
+const typed = new Typed(".typed", {
+  strings: [
+    '<i class="habilidad">Docente Universitario</i>',
+    '<i class="habilidad">Desarrollador Backend</i>',
+    '<i class="habilidad">Desarrollador Frontend</i>',
+  ],
+  stringsElement: "#cadenas-texto", // ID del elemento que contiene cadenas de texto a mostrar.
+  typeSpeed: 75, // Velocidad en mlisegundos para poner una letra,
+  startDelay: 300, // Tiempo de retraso en iniciar la animacion. Aplica tambien cuando termina y vuelve a iniciar,
+  backSpeed: 75, // Velocidad en milisegundos para borrrar una letra,
+  smartBackspace: true, // Eliminar solamente las palabras que sean nuevas en una cadena de texto.
+  shuffle: false, // Alterar el orden en el que escribe las palabras.
+  backDelay: 1500, // Tiempo de espera despues de que termina de escribir una palabra.
+  loop: true, // Repetir el array de strings
+  loopCount: false, // Cantidad de veces a repetir el array.  false = infinite
+  showCursor: true, // Mostrar cursor palpitanto
+  cursorChar: "|", // Caracter para el cursor
+  contentType: "html", // 'html' o 'null' para texto sin formato
+});
+
+let menuVisible = false;
+//Función que oculta o muestra el menu
+function mostrarOcultarMenu() {
+  if (menuVisible) {
+    document.getElementById("nav").classList = "";
+    menuVisible = false;
+  } else {
+    document.getElementById("nav").classList = "responsive";
+    menuVisible = true;
+  }
+}
+
+function seleccionar() {
+  //oculto el menu una vez que selecciono una opcion
+  document.getElementById("nav").classList = "";
+  menuVisible = false;
+}
+//Funcion que aplica las animaciones de las habilidades
+function efectoHabilidades() {
+  var skills = document.getElementById("skills");
+  var distancia_skills =
+    window.innerHeight - skills.getBoundingClientRect().top;
+  if (distancia_skills >= 300) {
+    let habilidades = document.getElementsByClassName("progreso");
+    habilidades[0].classList.add("javascript");
+    habilidades[1].classList.add("htmlcss");
+    habilidades[2].classList.add("photoshop");
+    habilidades[3].classList.add("wordpress");
+    habilidades[4].classList.add("drupal");
+    habilidades[5].classList.add("comunicacion");
+    habilidades[6].classList.add("trabajo");
+    habilidades[7].classList.add("creatividad");
+    habilidades[8].classList.add("dedicacion");
+    habilidades[9].classList.add("proyect");
+  }
+}
+
+//detecto el scrolling para aplicar la animacion de la barra de habilidades
+window.onscroll = function () {
+  efectoHabilidades();
+};
+
+//Función para filtrar las categorías de los trabajos
+function verCategoria(cat) {
+  const items = document.getElementsByClassName("item");
+  for (let i = 0; i < items.length; i++) {
+    items[i].style.display = "none";
+  }
+
+  const itemCat = document.getElementsByClassName(cat);
+  for (let i = 0; i < itemCat.length; i++) {
+    itemCat[i].style.display = "block";
+  }
+
+  const links = document.querySelectorAll(".trabajos nav a");
+  links[0].className = "";
+  links[1].className = "";
+  links[2].className = "";
+  links[3].className = "";
+
+  const itemSeleccionado = document.getElementById(cat);
+  itemSeleccionado.className = "borde";
+}
